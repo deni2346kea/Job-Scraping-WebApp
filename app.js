971c08b24cc365e67f6fc62f5b8aa56a038ecd78
Denis Kutnár <deni2346@stud.kea.dk>
2019-04-03 19:17:46 +0200

    Search engine

--- app.js
+++ app.js
@@ -16,22 +16,29 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', '.hbs');
 
 app.get('/', function (req, res) {
-    let url = `https://indreed.herokuapp.com/api/jobs?q=web+developer&limit=50`;
-    axios({
-        method: 'get',
-        url
+    res.render("index", {title:"Jobby"});
+});
+
+app.get('/search', function (req, res) {
+    queries = req.query;
+    let url = `https://indreed.herokuapp.com/api/jobs`;
+    if (queries){
+        axios.get(url, {
+        params: queries
     })
-    .then(function (response) {
-        let jobs = response.data;
-        res.render("index", { title: "Denis", jobs: jobs});
+    .then(function(response){
+        res.render("search", { title: "Jobby", jobs: response.data});
     })
-    .catch(function (error) {
+    .catch(function(error) {
         console.log(error);
     });
-
+    }
+    else {
+        res.render("search", {title: "Jobby"})
+    }
 });
 
 app.listen(app.get('PORT'), function () {
     console.log('Express started on http://localhost:' +
-        app.get('PORT') + '; press Cmd-C to terminate.');
+        app.get('PORT') + '; press Ctrl-C to terminate.');
 });
